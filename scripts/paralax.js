12f29739260c6d284926a76979e3e405b96d7dde
Agostina Tapia <agostinatapia98@gmail.com>
2024-11-30 20:46:38 -0300

    pag prin

--- scripts/paralax.js
+++ scripts/paralax.js
@@ -1,29 +1,5 @@
-//Registro e inicio de sesion 
-document.getElementById ("btn_registrase").addEventListener ("click", registro)
-document.getElementById ("btn_iniciar_sesion").addEventListener ("click", iniciar)
-//variables utilizadas
-var contenedor_login_registro = document.querySelector(".contenedor_login_registro")
-var formulario_login = document.querySelector(".formulario_login")
-var formulario_registro = document.querySelector(".formulario_registro")
-var caja_registro = document.querySelector(".caja_trasera_registro")
-var caja_login = document.querySelector(".caja_trasera_login")
-
-function iniciar(){
-    formulario_registro.style.display= "none"; 
-    contenedor_login_registro.style.left= "10px"; 
-    formulario_login.style.display= "block";
-    caja_registro.style.opacity= "1"; 
-    caja_login.style.opacity= "0"; 
-}
-
-function registro(){
-    formulario_registro.style.display= "block"; 
-    contenedor_login_registro.style.left= "410px"; 
-    formulario_login.style.display= "none";
-    caja_registro.style.opacity= "0"; 
-    caja_login.style.opacity= "1"; 
-}
-// CARRUSEL
+// CARRUSEL CÓMO FUNCIONA
+// USUARIO
 let indice = 0;
 
 function mostrarImagen() {
@@ -62,27 +38,30 @@ function mostrar(){
     }
 }
 
+
+//CARRUSEL PROFESIONAL
 let ind = 0;
 
 function mostrarImg() {
-    const imagenes = document.querySelectorAll('.carruselprof .slider-wraper .sliderprof div');
+    const imagenes = document.querySelectorAll('.carruselprof .slider-wraper-prof .sliderprof div');
     imagenes.forEach((div, i) => {
         div.style.display = 'none';
     });
     imagenes[ind].style.display = 'block';
 }
 
-function cambiarImg(direccion) {
-    const imagenes = document.querySelectorAll('.carruseluser .slider-wraper .slider div');
-    ind += direccion;
-    
+function cambiarImg(direcc) {
+    const imagenes = document.querySelectorAll('.carruselprof .slider-wraper-prof .sliderprof div');
+    ind += direcc;
+
     if (ind >= imagenes.length) {
-        indice = 0;
-    } else if (indice < 0) {
+        ind = 0;
+    } else if (ind < 0) {
         ind = imagenes.length - 1;
     }
-
-    mostrarImagen();
+    mostrarImg();
 }
 
 
+// Inicializa el carrusel mostrando la primera imagen
+mostrarImg();
