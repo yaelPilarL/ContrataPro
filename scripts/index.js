3fe03089c6c57182ae2d5fc85f949fe66d777cb2
Agustin Nazaretto <agustin_nazaretto@hotmail.com>
2024-12-05 21:18:14 -0300

    correcion camino feliz, y responsive registro

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 //Registro e inicio de sesion 
 document.getElementById ("btn_registrase").addEventListener ("click", registro)
 document.getElementById ("btn_iniciar_sesion").addEventListener ("click", iniciar)
+window.addEventListener("resize", anchoPage);
 //variables utilizadas
 var contenedor_login_registro = document.querySelector(".contenedor_login_registro")
 var formulario_login = document.querySelector(".formulario_login")
@@ -8,18 +9,51 @@ var formulario_registro = document.querySelector(".formulario_registro")
 var caja_registro = document.querySelector(".caja_trasera_registro")
 var caja_login = document.querySelector(".caja_trasera_login")
 
+function anchoPage(){
+    if (window.innerWidth > 850){
+        caja_registro.style.display = "block";
+        caja_login.style.display = "block";
+    }else{
+        caja_registro.style.display = "block";
+        caja_registro.style.opacity = "1";
+        caja_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_registro.style.left = "0px";
+        formulario_registro.style.display = "none";   
+    }
+}
+anchoPage();
+
 function iniciar(){
-    formulario_registro.style.display= "none"; 
+    if(window.innerWidth > 850){
+    formulario_login.style.display= "block"; 
     contenedor_login_registro.style.left= "10px"; 
-    formulario_login.style.display= "block";
+    formulario_registro.style.display= "none";
     caja_registro.style.opacity= "1"; 
     caja_login.style.opacity= "0"; 
+    }else{
+    formulario_login.style.display= "block"; 
+    contenedor_login_registro.style.left= "0px"; 
+    formulario_registro.style.display= "none";
+    caja_registro.style.display= "block"; 
+    caja_login.style.display= "none"; 
+    }
 }
 
 function registro(){
+    if(window.innerWidth > 850){
     formulario_registro.style.display= "block"; 
     contenedor_login_registro.style.left= "410px"; 
     formulario_login.style.display= "none";
     caja_registro.style.opacity= "0"; 
     caja_login.style.opacity= "1"; 
+    }else{
+    formulario_registro.style.display= "block"; 
+    contenedor_login_registro.style.left= "0px"; 
+    formulario_login.style.display= "none";
+    caja_registro.style.display= "none"; 
+    caja_login.style.display= "block"
+    caja_login.style.opacity= "1"; 
+
+}
 }
